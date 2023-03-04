c2381a56d953af54cdb1cb8a8b6f6d0adaa07173
rendznicoy <21-1-01027@vsu.edu.ph>
2023-03-05 07:58:31 +0800

    Update lang sa

--- OnlineTicketReservationApplication/Agent.cs
+++ OnlineTicketReservationApplication/Agent.cs
@@ -4,26 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Agent
+namespace OnlineTicketReservationApplication
 {
-	public string Name { get; private set; }
-	public string Email { get; private set; }
-	public int ContactNumber { get; private set; }
-	public Agent(string name, string email, int contactNumber)
-	{
-		this.Name = name;
-		this.Email = email;
-		this.ContactNumber = contactNumber;
-	}
+    public class Agent
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public int ContactNumber { get; private set; }
+        public Agent(string name, string email, int contactNumber)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.ContactNumber = contactNumber;
+        }
 
-	public void AddTicket()
-	{
+        public void AddTicket()
+        {
 
-	}
+        }
 
-	public override string ToString()
-	{
-		return this.Name + '\n' + this.Email + '\n' + this.ContactNumber + '\n';
+        public override string ToString()
+        {
+            return this.Name + '\n' + this.Email + '\n' + this.ContactNumber + '\n';
 
+        }
     }
 }
