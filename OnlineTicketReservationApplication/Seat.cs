3ddfe2c9dc9784b0842d1db871e10b09da041da4
rendznicoy <21-1-01027@vsu.edu.ph>
2023-03-05 12:49:11 +0800

    Added Class Seat. Still a work in progress. My brain is log

---
+++ OnlineTicketReservationApplication/Seat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineTicketReservationApplication
+{
+    public class Seat
+    {
+        public List<Ticket> Ticket { get; private set; }
+    }
